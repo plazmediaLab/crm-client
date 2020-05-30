f6fb326ae38fc0327ab942ca627cba87427bacba
Adrian Nieves Uribe <contacto.plazmedia@gmail.com>
2020-05-29 19:37:42 -0500

    Configuración base, antes de Tailwind Styled C

---
+++ src/components/asideBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+// Styled Component
+import { Title } from './styled component/styled';
+
+export default function AsideBar(){
+  return (
+    <aside className="bg-indigo-10 txt-w p-2">
+
+      <Title>CRM Clients</Title>
+
+    </aside>
+  );
+};
--- src/components/seo.jsx
+++ src/components/seo.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 
 import { ModularCSSfonstRequired } from 'modular-css';
+import Favicon from '../images/isotype-white-64px.png';
 
 export default function Seo(){
   return (
     <Helmet>
       <title>CRM-CLIENT</title>
       <link href={ModularCSSfonstRequired} rel="stylesheet" />
+      <link rel="icon" href={ Favicon }/>
+      <meta name="theme-color" content="#070707" />
     </Helmet>
   );
 };
