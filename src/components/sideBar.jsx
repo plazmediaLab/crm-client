340d0c2c260bc7618d2f842547eb2482ee790b34
Adrian Nieves Uribe <contacto.plazmedia@gmail.com>
2020-05-31 15:11:01 -0500

    Diseño sidebar modificado

--- src/components/sideBar.jsx
+++ src/components/sideBar.jsx
@@ -1,6 +1,19 @@
 import React from 'react';
 // Reach Router
 import { Link } from '@reach/router';
+// Styled Components 
+import styled from '@emotion/styled';
+import logo from '../images/imagotype-white-transparent.svg'
+
+const Logo = styled.div`
+  display: block;
+  width: auto;
+  height: 22px;
+  background-image: url(${logo});
+  background-size: contain;
+  background-repeat: no-repeat;
+  margin-bottom: 1rem;
+`;
 
 const NavLink = props => (
   <Link
@@ -17,10 +30,12 @@ const NavLink = props => (
 
 export default function SideBar(){
   return (
-    <aside className="bg-gray-800 sm:w-1/3 xl:w-1/5 sm:min-h-screen p-3 pl-5">
+    <aside className="bg-gray-800 sm:w-1/3 xl:w-1/5 sm:min-h-screen p-4">
 
       <div>
-        <h1 className="text-2xl text-white font-bold">CRM Clients</h1>
+        <Logo />
+        <hr className="border-gray-700 my-2"/>
+        <h1 className="text-lg text-white font-medium uppercase">(CRM) Administrator</h1>
       </div>
 
       <nav className="mt-5 list-none">
